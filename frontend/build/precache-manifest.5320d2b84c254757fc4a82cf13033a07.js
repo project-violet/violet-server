f201d61f1d0853ba04efbaeb343ae1c1add37ebf
violet-dev <violet.dev.master@gmail.com>
2020-09-06 13:30:37 +0900

    (frontend) Add test frontend

---
+++ frontend/build/precache-manifest.5320d2b84c254757fc4a82cf13033a07.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c60196bcd3bff591c34667b7d99f1143",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af7d52daa9c85af4b9c4",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c55014c4c6c9d7545ffd",
+    "url": "/static/js/2.67a64a32.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.67a64a32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af7d52daa9c85af4b9c4",
+    "url": "/static/js/main.293ac8b5.chunk.js"
+  },
+  {
+    "revision": "d01bbe22965da4f21af9",
+    "url": "/static/js/runtime-main.fc443553.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
