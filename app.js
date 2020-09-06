1c342d2396437fe08629f03ffadaf64b0b23843c
violet-dev <violet.dev.master@gmail.com>
2020-09-06 09:53:26 +0900

    (server) Preventing abnormal requests

--- app.js
+++ app.js
@@ -10,10 +10,33 @@
 
 const express = require('express');
 const createError = require('http-errors');
+const expressDefend = require('express-defend');
+const blacklist = require('express-blacklist');
+const rateLimit = require("express-rate-limit");
 
 const r_api = require('./routes/api');
 
 const app = express();
+
+// Ban ip address.
+app.use(blacklist.blockRequests('blacklist.txt'));
+app.use(expressDefend.protect({ 
+    maxAttempts: 5,
+    dropSuspiciousRequest: true,
+    logFile: 'suspicious.log',
+    onMaxAttemptsReached: function(ipAddress, url){
+      blacklist.addAddress(ipAddress);
+    } 
+}));
+
+// Limit Request
+const limiter = rateLimit({
+  windowMs: 10000,
+  max: 10,
+  message:
+    "Too many accounts created from this IP, please try again after an hour"
+});
+app.use(limiter);
 
 app.use('/', r_api);
 
