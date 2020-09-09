ce4b328efc18306eae5a632d85c6ec324853b433
violet-dev <violet.dev.master@gmail.com>
2020-09-09 17:50:49 +0900

    (server) Update status pages

--- routes/top.js
+++ routes/top.js
@@ -2,6 +2,7 @@
 // Copyright (C) 2020. violet-team. Licensed under the Apache-2.0 License.
 
 var m_view = require("../memory/view");
+var p = require("../pages/status");
 
 // Gets the tags counted with the view function.
 module.exports = async function top(req, res, next) {
@@ -10,13 +11,13 @@ module.exports = async function top(req, res, next) {
   var type = req.query.type || 'daily';
 
   if (offset == null || count == null || isNaN(offset) || isNaN(count)) {
-    res.status(400).type('text').send('Use like https://koromo.xyz/top?offset=0&count=100&type={daily|week|month|alltime}');
+    res.status(400).type("html").send(p.p400);
     return;
   }
 
   var result = m_view.query(offset, count, type);
   if (result == null)
-    res.status(400).type('text').send('Bad query.');
+    res.status(400).type("html").send(p.p400);
   else 
     res.type('json').send({'msg':'success', 'result': result});
 }; 
