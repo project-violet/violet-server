453b480846a0b3573b9310fa55c58ac8245e772a
violet-dev <violet.dev.master@gmail.com>
2021-02-11 01:46:12 +0000

    (view) Implements key expired event

--- memory/view_redis.js
+++ memory/view_redis.js
@@ -1,16 +1,18 @@
 // This source code is a part of Project Violet.
 // Copyright (C) 2021. violet-team. Licensed under the Apache-2.0 License.
 
-const a_database = require("../api/database");
-const a_syncdatabase = require("../api/syncdatabase");
+const a_database = require('../api/database');
+const a_syncdatabase = require('../api/syncdatabase');
 
-const logger = require("../etc/logger");
-const redis = require("../api/redis");
-const redis_sub = require("../api/redis_sub");
+const logger = require('../etc/logger');
+const redis = require('../api/redis');
+const redis_sub = require('../api/redis_sub');
 
 function init() {
   var conn = a_syncdatabase();
-  var count = conn.query("SELECT COUNT(*) AS C FROM viewtotal")[0]['C'];
+  var count = conn.query('SELECT COUNT(*) AS C FROM viewtotal')[0]['C'];
+
+  console.log('[init] ' + count + ' datas ready to load.');
 
   const load_per = 50000;
   var offset = 0;
@@ -18,16 +20,17 @@ function init() {
   var now = new Date();
 
   for (;;) {
-    var qr = conn.query("SELECT * FROM viewtotal ORDER BY Id DESC LIMIT " + load_per.toString() + " OFFSET " + offset.toString());
-    data = qr;
-
+    var data = conn.query(
+        'SELECT * FROM viewtotal ORDER BY Id DESC LIMIT ' +
+        load_per.toString() + ' OFFSET ' + offset.toString());
+        
     for (var i = 0; i < data.length; i++) {
       var diff = now - new Date(data[i].TimeStamp);
-      
+
       redis.zincrby('alltime', 1, data[i].ArticleId);
-  
-      var key_name = data[i].ArticleId + i.toString();
-      
+
+      var key_name = data[i].ArticleId + '-' + i.toString();
+
       if (diff < 1 * 1000 * 60 * 60 * 24 && diff > 0) {
         redis.zincrby('daily', 1, data[i].ArticleId);
         redis.set('daily-' + key_name, 1);
@@ -46,14 +49,18 @@ function init() {
     }
 
     offset += load_per;
-    if (offset < count) break;
+    console.log(
+        '[init] ' + offset + '/' + count + ' (' + (offset / count * 100) +
+        '%)');
+    if (offset > count || data.length == 0) break;
   }
 }
 
-redis_sub.psubscribe('*').then(function (e) {
+redis_sub.psubscribe('*').then(function(e) {
   redis_sub.on('pmessage', function(pattern, message, channel) {
-    if (message.toString().endsWith('expired')) {
-      redis.zincrby(channel, -1)
+    if (message.toString().startsWith('__keyevent') &&
+        message.toString().endsWith('expire')) {
+      redis.zincrby(channel.split('-')[0], -1, channel.split('-')[1]);
     }
   });
 });
@@ -62,7 +69,7 @@ init();
 
 function append(no) {
   var now = new Date();
-  var key_name = now + no.toString();
+  var key_name = no.toString() + '-' + now;
 
   redis.zincrby('alltime', 1, no);
 
@@ -80,50 +87,47 @@ function append(no) {
 }
 
 module.exports = {
-    append: function (no, userid) {
-      try {
-        logger.info('view-append %d %s', no, userid);
-        append(no);
-      } catch (e) {
-        logger.error('view-append');
-        logger.error(e);
-        console.log(e);
-      }
-  
-      var pool = a_database();
-      pool.query(
-        "INSERT INTO viewtotal SET ?",
-        {
+  append: function(no, userid) {
+    try {
+      logger.info('view-append %d %s', no, userid);
+      append(no);
+    } catch (e) {
+      logger.error('view-append');
+      logger.error(e);
+      console.log(e);
+    }
+
+    var pool = a_database();
+    pool.query(
+        'INSERT INTO viewtotal SET ?', {
           ArticleId: no,
           TimeStamp: CURRENT_TIMESTAMP,
           UserAppId: userid,
         },
-        function (error, results, fields) {
+        function(error, results, fields) {
           if (error != null) {
-            logger.error("viewdb", error);
+            logger.error('viewdb', error);
           }
-        }
-      );
-    },
-  
-    query: function (offset, count, type) {
-       return new Promise(function(resolve, reject) {
-        switch (type) {
-          case "daily":
-            resolve(daily.query(offset, count).map((e) => [e.k, e.v]));
-            break;
-          case "week":
-            resolve(week.query(offset, count).map((e) => [e.k, e.v]));
-            break;
-          case "month":
-            resolve(month.query(offset, count).map((e) => [e.k, e.v]));
-            break;
-          case "alltime":
-            resolve(alltime.query(offset, count).map((e) => [e.k, e.v]));
-            break;
-        }
-        resolve(null);
-      });
-    },
-  };
-  
+        });
+  },
+
+  query: function(offset, count, type) {
+    return new Promise(function(resolve, reject) {
+      switch (type) {
+        case 'daily':
+          resolve(daily.query(offset, count).map((e) => [e.k, e.v]));
+          break;
+        case 'week':
+          resolve(week.query(offset, count).map((e) => [e.k, e.v]));
+          break;
+        case 'month':
+          resolve(month.query(offset, count).map((e) => [e.k, e.v]));
+          break;
+        case 'alltime':
+          resolve(alltime.query(offset, count).map((e) => [e.k, e.v]));
+          break;
+      }
+      resolve(null);
+    });
+  },
+};
