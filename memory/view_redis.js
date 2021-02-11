b702598a19be8a0d78b1da52b4349da277189287
violet-dev <violet.dev.master@gmail.com>
2021-02-11 04:48:32 +0000

    (memory) Split server memory initializer

--- memory/view_redis.js
+++ memory/view_redis.js
@@ -8,63 +8,6 @@ const logger = require('../etc/logger');
 const redis = require('../api/redis');
 const redis_sub = require('../api/redis_sub');
 
-function init() {
-  if (redis.get('initialized') == 1)
-    return;
-
-  redis.flushall();
-  redis.setMaxListeners(0);
-
-  var conn = a_syncdatabase();
-  var count = conn.query('SELECT COUNT(*) AS C FROM viewtotal')[0]['C'];
-
-  console.log('[init] ' + count + ' datas ready to load.');
-
-  const load_per = 5000;
-  var offset = 0;
-
-  var now = new Date();
-
-  for (;;) {
-    var data = conn.query(
-        'SELECT * FROM viewtotal ORDER BY Id DESC LIMIT ' +
-        load_per.toString() + ' OFFSET ' + offset.toString());
-
-    for (var i = 0; i < data.length; i++) {
-      var diff = now - new Date(data[i].TimeStamp);
-
-      redis.zincrby('alltime', 1, data[i].ArticleId);
-
-      var key_name = data[i].ArticleId + '-' + i.toString();
-
-      if (diff < 1 * 1000 * 60 * 60 * 24 && diff > 0) {
-        redis.zincrby('daily', 1, data[i].ArticleId);
-        redis.set('daily-' + key_name, 1);
-        redis.expire('daily-' + key_name, diff);
-      }
-      if (diff < 7 * 1000 * 60 * 60 * 24 && diff > 0) {
-        redis.zincrby('weekly', 1, data[i].ArticleId);
-        redis.set('weekly-' + key_name, 1);
-        redis.expire('weekly-' + key_name, diff);
-      }
-      if (diff < 30 * 1000 * 60 * 60 * 24 && diff > 0) {
-        redis.zincrby('monthly', 1, data[i].ArticleId);
-        redis.set('monthly-' + key_name, 1);
-        redis.expire('monthly-' + key_name, diff);
-      }
-    }
-
-    offset += load_per;
-    console.log(
-        '[init] ' + offset + '/' + count + ' (' + (offset / count * 100) +
-        '%)');
-    if (offset > count || data.length == 0) break;
-  }
-
-  redis.set('initialized', 1);
-  redis.set('latest-update', new Date().toString());
-}
-
 redis_sub.psubscribe('*').then(function(e) {
   redis_sub.on('pmessage', function(pattern, message, channel) {
     if (message.toString().startsWith('__keyevent') &&
@@ -74,8 +17,6 @@ redis_sub.psubscribe('*').then(function(e) {
     }
   });
 });
-
-init();
 
 function append(no) {
   var now = new Date();
---
+++ server-initializer.js
@@ -0,0 +1,87 @@
+//===----------------------------------------------------------------------===//
+//
+//                       Violet API Server Initializer
+//
+//===----------------------------------------------------------------------===//
+//
+//  Copyright (C) 2021. violet-team. All Rights Reserved.
+//
+//===----------------------------------------------------------------------===//
+
+const a_syncdatabase = require('./api/syncdatabase');
+
+const redis = require('./api/redis');
+
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+async function init() {
+  redis.get('initialized', async (err, reply) => {
+    if (reply == '1') {
+        console.log('[init] Memory is already ready!')
+        //return;
+    }
+
+    redis.flushall();
+
+    var conn = a_syncdatabase();
+    var count = conn.query('SELECT COUNT(*) AS C FROM viewtotal')[0]['C'];
+
+    console.log('[init] ' + count + ' datas ready to load.');
+
+    const load_per = 5000;
+    var offset = 0;
+
+    var now = new Date();
+
+    for (;;) {
+      var data = conn.query(
+          'SELECT * FROM viewtotal ORDER BY Id DESC LIMIT ' +
+          load_per.toString() + ' OFFSET ' + offset.toString());
+
+      for (var i = 0; i < data.length; i++) {
+        var diff = now - new Date(data[i].TimeStamp);
+
+        redis.zincrby('alltime', 1, data[i].ArticleId);
+
+        var key_name = data[i].ArticleId + '-' + i.toString();
+
+        if (diff < 1 * 1000 * 60 * 60 * 24 && diff > 0) {
+          redis.zincrby('daily', 1, data[i].ArticleId);
+          redis.set('daily-' + key_name, 1);
+          redis.expire('daily-' + key_name, diff);
+        }
+        if (diff < 7 * 1000 * 60 * 60 * 24 && diff > 0) {
+          redis.zincrby('weekly', 1, data[i].ArticleId);
+          redis.set('weekly-' + key_name, 1);
+          redis.expire('weekly-' + key_name, diff);
+        }
+        if (diff < 30 * 1000 * 60 * 60 * 24 && diff > 0) {
+          redis.zincrby('monthly', 1, data[i].ArticleId);
+          redis.set('monthly-' + key_name, 1);
+          redis.expire('monthly-' + key_name, diff);
+        }
+      }
+
+      await sleep(200);
+
+      offset += load_per;
+      console.log(
+          '[init] ' + offset + '/' + count + ' (' + (offset / count * 100) +
+          '%)');
+      if (offset > count || data.length == 0) break;
+    }
+
+    redis.set('initialized', '1');
+    redis.set('latest-update', new Date().toString());
+
+    await sleep(200);
+
+    console.log('[init] Server is initialized successfully!');
+  });
+}
+
+init();
