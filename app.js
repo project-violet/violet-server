fe3548a11c3ba490b3d1be6a1ce3dc97f800ed85
violet-dev <violet.dev.master@gmail.com>
2021-03-27 15:17:51 +0000

    (app) Revert deleting router

--- app.js
+++ app.js
@@ -15,9 +15,12 @@ const blacklist = require("express-blacklist");
 const rateLimit = require("express-rate-limit");
 
 const r_community = require('./routes/community/routes');
+const r_index = require("./routes/index");
 const r_query = require("./routes/query");
 const r_top = require("./routes/top");
 const r_upload = require("./routes/upload");
+const r_view = require("./routes/view");
+const r_view_close = require("./routes/view_close");
 
 const t_1144 = require("./routes/test1144");
 const t_1145 = require("./routes/test1145");
@@ -59,6 +62,10 @@ app.use("/community", r_community);
 app.use("/query", r_query);
 app.use("/top", r_top);
 app.post("/upload", r_upload);
+app.post("/view", r_view);
+app.get("/view", function (req, res, next) { res.status(405).type("html").send(p.p405); });
+app.post("/view_close", r_view_close);
+app.get("/view_close", function (req, res, next) { res.status(405).type("html").send(p.p405); });
 
 app.use("/1144", t_1144);
 app.use("/1145", t_1145);
