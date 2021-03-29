d6b9a3ce207cba1f170b4c6a5a9dd1c4280b9c30
violet-dev <violet.dev.master@gmail.com>
2021-03-29 04:15:53 +0000

    (models) Add models prototype

---
+++ models/article.js
@@ -0,0 +1,11 @@
+module.exports = {
+  insert: async function insert(board, user, title, body, etc) {
+
+  },
+  read: async function read(id) {
+
+  },
+  exists: async function exists(id) {
+
+  },
+};
