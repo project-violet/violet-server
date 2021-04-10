d6c84d328696f3754bed1fc688f858b066c9791e
violet-dev <violet.dev.master@gmail.com>
2021-04-10 07:07:41 +0000

    (search) Implements 1-step search

---
+++ routes/search/routes.js
@@ -0,0 +1,17 @@
+// This source code is a part of Project Violet.
+// Copyright (C) 2020-2021. violet-team. Licensed under the Apache-2.0 License.
+
+const express = require('express');
+const router = express.Router();
+const p = require('../../pages/status');
+
+function p405(req, res, next) {
+  res.status(405).type('html').send(p.p405);
+}
+
+const main = require('./main');
+
+router.post('/main', main);
+router.get('/main', p405);
+
+module.exports = router;
