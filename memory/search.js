96d70f53adf15c6cd9df7624da77f206d2908f25
violet-dev <violet.dev.master@gmail.com>
2021-04-11 05:04:42 +0000

    (search) Implements 2-step search

--- memory/search.js
+++ memory/search.js
@@ -145,7 +145,9 @@ function _makeTree(what) {
   }
 
   _next();
-  return _expr();
+  var t = _expr();
+  if (t.op != 'and') t = new _treeNode([t], null, 'and');
+  return t;
 }
 
 //
@@ -178,100 +180,233 @@ function _printTree(node) {
   console.log(x.s);
 }
 
-function _alignTreeNodeChilds(node) {
-  if (node.contents.length == 0) return;
+function _optimizeTree(node) {
+  /*
+  Align Child Item for High Performance Query
+  */
+  function _alignTree(node) {
+    if (node.contents.length == 0) return;
 
-  function _compareContent(c1, c2) {
-    //
-    // Global Priority
-    // nt => and => exclude => or
-    //
-    if (c1.op == c2.op) return 0;
-    var x = c1.op == '-' || c1.op == 'or' || c1.op == 'and';
-    var y = c2.op == '-' || c2.op == 'or' || c2.op == 'and';
+    function _compareContent(c1, c2) {
+      //
+      // Global Priority
+      // nt => and => exclude => or
+      //
+      if (c1.op == c2.op) {
+        if (c1.op == '-' && c2.op == '-') {
+          // Compare with Contents
+          return _compareContent(c1.contents[0], c2.contents[0]);
+        }
 
-    //
-    // Compare Content
-    // tag (ar => dr) => title
-    //
-    if (!x && !y) {
-      //
-      // Compare Is Token Or TitleText
-      //
-      var tt1 = c1.op.includes(':');
-      var tt2 = c2.op.includes(':');
-      if (!tt1 && !tt2) return 0;
-      if (tt1 != tt2) return tt1 ? -1 : 1;
+        return 0;
+      }
+      var x = c1.op == '-' || c1.op == 'or' || c1.op == 'and';
+      var y = c2.op == '-' || c2.op == 'or' || c2.op == 'and';
 
       //
-      // Compare Allow Redundancy
+      // Compare Content
+      // tag (ar => dr) => title
       //
-      if (tokenType[c1.op.split(':')[0]][0] > tokenType[c2.op.split(':')[0]][0])
-        return -1;
-      else if (
-          tokenType[c1.op.split(':')[0]][0] < tokenType[c2.op.split(':')[0]][0])
-        return 1;
+      if (!x && !y) {
+        //
+        // Compare Is Token Or TitleText
+        //
+        var tt1 = c1.op.includes(':');
+        var tt2 = c2.op.includes(':');
+        if (!tt1 && !tt2) return 0;
+        if (tt1 != tt2) return tt1 ? -1 : 1;
 
-      //
-      //  Compare with Counts (lower is unique)
-      //
-      var x = tagCountMap[c1.op];
-      var y = tagCountMap[c2.op];
-      if ((x == null) != (y == null)) return x == null ? -1 : 1;
-      if (x == null && y == null) return 0;
-      if (x < y)
-        return -1;
-      else if (x > y)
-        return 1;
+        //
+        // Compare Allow Redundancy
+        //
+        if (tokenType[c1.op.split(':')[0]][0] >
+            tokenType[c2.op.split(':')[0]][0])
+          return -1;
+        else if (
+            tokenType[c1.op.split(':')[0]][0] <
+            tokenType[c2.op.split(':')[0]][0])
+          return 1;
+
+        //
+        //  Compare with Counts (lower is unique)
+        //
+        var x = tagCountMap[c1.op];
+        var y = tagCountMap[c2.op];
+        if ((x == null) != (y == null)) return x == null ? -1 : 1;
+        if (x == null && y == null) return 0;
+        if (x < y)
+          return -1;
+        else if (x > y)
+          return 1;
+        return 0;
+      }
+      if (!x) return -1;
+      if (!y) return 1;
+      if (c1.op == 'and') return -1;
+      if (c2.op == 'and') return 1;
+      if (c1.op == '-') return -1;
+      if (c2.op == '-') return 1;
       return 0;
     }
-    if (!x) return -1;
-    if (!y) return 1;
-    if (c1.op == 'and') return -1;
-    if (c2.op == 'and') return 1;
-    if (c1.op == '-') return -1;
-    if (c2.op == '-') return 1;
-    return 0;
-  }
 
-  function _nodeContentsSwap(node, p1, p2) {
-    var tmp = node.contents[p1];
-    node.contents[p1] = node.contents[p2];
-    node.contents[p2] = tmp;
-  }
-
-  //
-  //  Selection Sort
-  //
-  for (var i = 0; i < node.contents.length - 1; i++) {
-    var least = i;
-    for (var j = i + 1; j < node.contents.length; j++) {
-      if (_compareContent(node.contents[j], node.contents[least]) < 0)
-        least = j;
+    function _nodeContentsSwap(node, p1, p2) {
+      var tmp = node.contents[p1];
+      node.contents[p1] = node.contents[p2];
+      node.contents[p2] = tmp;
     }
 
-    if (least != i) _nodeContentsSwap(node, least, i);
+    //
+    //  Selection Sort
+    //
+    for (var i = 0; i < node.contents.length - 1; i++) {
+      var least = i;
+      for (var j = i + 1; j < node.contents.length; j++) {
+        if (_compareContent(node.contents[j], node.contents[least]) < 0)
+          least = j;
+      }
+
+      if (least != i) _nodeContentsSwap(node, least, i);
+    }
+    for (var i = 0; i < node.contents.length; i++) {
+      _alignTree(node.contents[i]);
+    }
   }
+
+  /*
+  Pull Sub Ands To Parent
+
+  +- and
+    |- artist:michiking
+    +- and
+      |- lang:korean
+      |- type:artistcg
+      +- and
+        |- class:c99
+        +- male:sole male
+
+      ==>
+
+  +- and
+    |- artist:michiking
+    |- lang:korean
+    |- type:artistcg
+    |- class:c99
+    +- male:sole male
+  */
+  function _pullSubAndOrNodesRecursize(node) {
+    for (var i = 0; i < node.contents.length; i++)
+      _pullSubAndOrNodesRecursize(node.contents[i]);
+    if (node.op == 'and') {
+      for (var i = 0; i < node.contents.length; i++) {
+        if (node.contents[i].op == 'and') {
+          for (var j = node.contents[i].contents.length - 1; j >= 0; j--) {
+            node.contents[i].contents[j].parent = node.parent;
+            node.contents.push(node.contents[i].contents[j]);
+          }
+          node.contents.splice(i--, 1);
+        }
+      }
+    } else if (node.op == 'or') {
+      for (var i = 0; i < node.contents.length; i++) {
+        if (node.contents[i].op == 'or') {
+          for (var j = node.contents[i].contents.length - 1; j >= 0; j--) {
+            node.contents[i].contents[j].parent = node.parent;
+            node.contents.push(node.contents[i].contents[j]);
+          }
+          node.contents.splice(i--, 1);
+        }
+      }
+    }
+  }
+
+  /*
+  Unwind Minus Operation
+
+  -(A or B)     ==>  -A and -B
+  -(A   -B)     ==>  -A  or  B
+  */
+  function _unwindMinus(node) {
+    // (A or B) => -A and -B
+    function _propagate(node) {
+      // -A => A
+      if (node.op == '-') {
+        node.op = node.contents[i].op;
+        node.contents = node.contents[i].contents;
+      }
+      // A or B or C => -A and -B and -C
+      else if (node.op == 'or') {
+        node.op = 'and';
+        for (var i = 0; i < node.contents.length; i++)
+          _propagate(node.contents[i]);
+      }
+      // A and B and C => -A or -B or -C
+      else if (node.op == 'and') {
+        node.op = 'or';
+        for (var i = 0; i < node.contents.length; i++)
+          _propagate(node.contents[i]);
+      }
+      // Non-Terminal
+      else {
+        var n = new _treeNode([], node, node.op);
+        node.op = '-';
+        node.contents = [n];
+      }
+    }
+    for (var i = 0; i < node.contents.length; i++)
+      _unwindMinus(node.contents[i]);
+    for (var i = 0; i < node.contents.length; i++) {
+      if (node.contents[i].op == '-') {
+        if (node.contents[i].contents[0].op != 'and' &&
+            node.contents[i].contents[0].op != 'or' &&
+            node.contents[i].contents[0].op != '-')
+          continue;
+        _propagate(node.contents[i].contents[0]);
+        node.contents[i].contents[0].parent = node.parent;
+        node.contents.push(node.contents[i].contents[0]);
+        node.contents.splice(i--, 1);
+      }
+    }
+  }
+
+  /*
+  Check Distributable
+
+  A and (B or C)          => (A and B) or (A and C)
+      (when c(A) < c(B) + c(C))
+  (A and B) or (A and C)  => A and (B or C)
+      (when )
+  */
+
+  /*
+  Check Inconsistent
+
+  A and -A    => false
+  A or  -A    => true
+  */
+
+  // Run Optimization Passes
+  _pullSubAndOrNodesRecursize(node);
+  _alignTree(node);
+  _unwindMinus(node);
+  _alignTree(node);
+  _pullSubAndOrNodesRecursize(node);
+  _alignTree(node);
 }
 
-function _alignTree(node) {
-  _alignTreeNodeChilds(node);
-  for (var i = 0; i < node.contents.length; i++) {
-    _alignTree(node.contents[i]);
-  }
-}
 
 /*
     Search To SQL
 
-    1. Split and Build Parse Tree
+    1. Build Parse Tree
     2. Align using Tag Count Map
+
+    WHERE IN vs INNER JOIN
  */
-function _searchToSQL(rawSearch, enableInjection = false) {
+function _innerNodeToSQL(node) {
   function _createRandomString() {
     var result = [];
-    var characters =
-        'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' +
+        'abcdefghijklmnopqrstuvwxyz0123456789';
     var charactersLength = characters.length;
     for (var i = 0; i < 4; i++) {
       result.push(
@@ -280,203 +415,128 @@ function _searchToSQL(rawSearch, enableInjection = false) {
     return result.join('');
   }
 
-  function _split(what) {
-    var result = [];
-    var ga = [];
-    var cur = '';
-    var _ga = 0;
-    for (var i = 0; i < what.length; i++) {
-      //   if (!_checkValid(what[i])) continue;
-      if (what[i] == '(') {
-        _ga += 1;
-      } else if (what[i] == ')') {
-        _ga -= 1;
-        if (cur != '' && _ga == 0) {
-          ga.push(cur);
-          cur = '';
-        }
-      } else if (what[i] == '-' && _ga == 0) {
-        if (cur != '') {
-          result.push(cur);
-          cur = '';
-        }
-        result.push('-');
-      } else if (what[i] == ' ' && _ga == 0) {
-        if (cur != '') {
-          result.push(cur);
-          cur = '';
-        }
-      } else {
-        cur += what[i];
-      }
-    }
-    if (cur != '') result.push(cur);
-    return [result, ga];
-  }
-
-  var split = _split(rawSearch);
-  var queryDesc = {
-    'artist': [],
-    'group': [],
-    'female': [],
-    'male': [],
-    'tag': [],
-    'lang': [],
-    'series': [],
-    'uploader': [],
-    'character': [],
-    'type': [],
-    'class': [],
-    'title': [],
-  };
-
-  for (var i = 0; i < split[0].length; i++) {
-    if (split[0][i].includes(':')) {
-      var ss = split[0][i].split(':');
-      var token = ss[0];
-      // unknown token type
-      if (!(token in tokenType)) continue;
-      if (i >= 1 && split[0][i - 1] == '-') {
-        if (token == 'female' || token == 'male')
-          queryDesc[token].push('-' + token + ':' + ss[1].replace('_', ' '));
-        else
-          queryDesc[token].push('-' + ss[1].replace('_', ' '));
-      } else {
-        if (token == 'female' || token == 'male')
-          queryDesc[token].push(token + ':' + ss[1].replace('_', ' '));
-        else
-          queryDesc[token].push(ss[1].replace('_', ' '));
-      }
-    } else if (split[0][i] == '-') {
-      continue;
-    } else {
-      // Title Token
-      if (i >= 1 && split[0][i - 1] == '-') {
-        queryDesc['title'].push('-' + split[0][i]);
-      } else {
-        queryDesc['title'].push(split[0][i]);
-      }
-    }
-  }
-
-  var innerQuery = '';
-  var outerQuery = '';
-  var requireJoin = false;
-
-  var innerExcepts = [];
-
+  var sql = '(select * from eharticles) as eh';
+  var wheres = [];
   var innerQValues = [];
   var outerQValues = [];
 
-  Object.entries(queryDesc).forEach(([key, value]) => {
-    if (value.length == 0) return;
+  for (var i = 0; i < node.contents.length; i++) {
+    var target = node.contents[i];
+    var neg = false;
 
-    if (tokenType[key][0] == 1) {
-      value.forEach((val => {
-        if (innerQuery != '') {
-          innerQuery += ' left join ';
-          requireJoin = true;
-        } else if (val.startsWith('-')) {
-          latestExcept = true;
-        }
-
-        var rs = _createRandomString();
-        innerQuery += '(select * from ' + tokenType[key][1] + ' as a ' +
-            'right join ' + tokenType[key][1] + '_junction as b on a.Id=b.' +
-            tokenType[key][2] + ' ' +
-            'where a.Name=?) as ' + key + rs;
-        if (requireJoin) {
-          innerQuery += ' on eh.Id=' + key + rs + '.Article';
-        } else {
-          if (val.startsWith('-'))
-            innerQuery =
-                '(select * from eharticles) as eh left join ' + innerQuery;
-          else
-            innerQuery =
-                '(select * from eharticles) as eh inner join ' + innerQuery;
-          innerQuery += ' on eh.Id=' + key + rs + '.Article';
-        }
-        latestTable = key + rs;
-        if (val.startsWith('-'))
-          innerExcepts.push(key + rs);
-        else
-          latestValidTable = key + rs;
-        innerQValues.push(val.startsWith('-') ? val.slice(1) : val);
-      }));
-    } else {
-      if (outerQuery != '') outerQuery += ' AND ';
-      if (key == 'title') {
-        outerQuery += 'match (Title) against (? in natural language mode)';
-      } else {
-        outerQuery += 'eh.' + tokenType[key][1] + '=?';
-      }
-      outerQValues.push(value);
+    if (target.op == 'or') {
+      wheres.push(
+          '(' +
+          node.contents[i]
+              .contents
+              .map(e => {
+                if (e.op == 'and' || e.op == 'or' || e.op == '-')
+                  throw 'Bad Query! Simple Query is accepted currently';
+                if (e.op.includes(':')) {
+                  var kv = e.op.split(':');
+                  if (kv[0] == 'female' || kv[0] == 'male')
+                    outerQValues.push(e.op.replace('_', ' '));
+                  else
+                    outerQValues.push(
+                        e.op.slice(e.op.indexOf(':') + 1).replace('_', ' '));
+                  return 'eh.' + tokenType[kv[0]][1] + '=?';
+                } else {
+                  outerQValues.push(e.op.replace('_', ' '));
+                  return 'match (eh.Title) against (? in natural language mode)';
+                }
+              })
+              .join(' or ') +
+          ')');
+      continue;
     }
+
+    if (target.op == '-') {
+      if (target.contents.length != 1) return;
+      target = target.contents[0];
+      neg = true;
+    }
+
+    if (target.op.includes(':')) {
+      var kv = target.op.split(':');
+      var rs = _createRandomString();
+
+      if (tokenType[kv[0]][0] == 1) {
+        var sub = '';
+
+        sub = '(select * from ' + tokenType[kv[0]][1] + ' as a ' +
+            'right join ' + tokenType[kv[0]][1] + '_junction as b on a.Id=b.' +
+            tokenType[kv[0]][2] + ' ' +
+            'where a.Name=?) as ' + kv[0] + rs;
+
+        sql += ' ' + (neg ? 'left' : 'inner') + ' join ' + sub +
+            ' on eh.Id=' + kv[0] + rs + '.Article';
+
+        if (neg) wheres.push(kv[0] + rs + '.Article IS NULL');
+
+        if (kv[0] == 'female' || kv[0] == 'male')
+          innerQValues.push(target.op.replace('_', ' '));
+        else
+          innerQValues.push(
+              target.op.slice(target.op.indexOf(':') + 1).replace('_', ' '));
+      } else {
+        wheres.push('eh.' + tokenType[kv[0]][1] + (neg ? '<>' : '=') + '?');
+        if (kv[0] == 'female' || kv[0] == 'male')
+          outerQValues.push(target.op.replace('_', ' '));
+        else
+          outerQValues.push(
+              target.op.slice(target.op.indexOf(':') + 1).replace('_', ' '));
+      }
+    } else {
+      wheres.push(
+          (neg ? 'not ' : '') +
+          'match (eh.Title) against (? in natural language mode)');
+      outerQValues.push(target.op);
+    }
+  }
+
+  if (wheres.length > 0) {
+    sql += ' where ' + wheres.join(' and ');
+  }
+
+  const front = 'select eh.Id from ';
+  const back = ' group by eh.Id order by eh.Id desc';
+  return [front + sql + back, innerQValues.concat(outerQValues)];
+}
+
+function _searchToSQL(rawSearch, enableInjection = false) {
+  var tree = _makeTree(rawSearch);
+  _optimizeTree(tree);
+  return _innerNodeToSQL(tree);
+}
+
+function _injectValues(sql, values) {
+  values.forEach(x => {
+    var i = sql.indexOf('?');
+    sql = sql.slice(0, i) + '"' +
+        x.replace('\\', '\\\\')
+            .replace('"', '\\"')
+            .replace('＼', '\\\\')
+            .replace('＂', '\\"') +
+        '"' + sql.slice(i + 1);
   });
-
-  if (innerExcepts.length > 0) {
-    innerQuery +=
-        ' where ' + innerExcepts.map(e => e + '.Article IS NULL').join(' and ');
-  }
-
-  var tq = '';
-
-  if (innerQuery != '' && outerQuery != '') {
-    tq = 'select eh.Id from ' + innerQuery +
-        (innerExcepts.length == 0 ? ' where ' : ' and ') + outerQuery +
-        ' group by eh.Id order by eh.Id desc';
-  } else if (innerQuery != '') {
-    tq = 'select eh.Id from ' + innerQuery +
-        ' group by eh.Id order by eh.Id desc';
-  } else if (outerQuery != '') {
-    tq = 'select Id from eharticles where ' + outerQuery +
-        ' group by Id order by Id desc';
-  }
-
-  function _checkValid(ch) {
-    return 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789:()-_ '
-        .includes(ch);
-  }
-
-  if (enableInjection) {
-    innerQValues.forEach(x => {
-      var i = tq.indexOf('?');
-      tq = tq.slice(0, i) + '"' +
-          x.replace('\\', '\\\\')
-              .replace('"', '\\"')
-              .replace('＼', '\\\\')
-              .replace('＂', '\\"') +
-          '"' + tq.slice(i + 1);
-    });
-
-    outerQValues.forEach(x => {
-      var y = '';
-      x.forEach(e => {
-        if (y != '') y += ',';
-        y += '"' +
-            e.replace('\\', '\\\\')
-                .replace('"', '\\"')
-                .replace('＼', '\\\\')
-                .replace('＂', '\\"') +
-            '"';
-      });
-      var i = tq.indexOf('?');
-      tq = tq.slice(0, i) + '(' + y + ')' + tq.slice(i + 1);
-    });
-
-    return tq;
-  }
-
-  return [tq, innerQValues.concat(outerQValues)];
+  return sql;
 }
 
 
 // _makeTree('-female:loli');
+// var tree = _makeTree(
+//     '-female:loli male:sole_male  (lang:korean or lang:n/a) artist:michiking -tag:incest');
 var tree = _makeTree(
-    '-female:loli male:sole_male  (lang:korean or lang:n/a) artist:michiking -tag:incest');
-_alignTree(tree);
-
+    ' lang:korean artist:michiking -(female:loli or male:shota or male:sole_male)');
+// var tree = _makeTree('lang:korean -female:loli');
+// var tree = _makeTree(
+//     'artist:michiking lang:korean -female:loli -male:shota
+//     -male:sole_male');
+_optimizeTree(tree);
 _printTree(tree);
+r = _innerNodeToSQL(tree);
+console.log(r[0]);
+console.log(_injectValues(r[0], r[1]));
 
 /*
 +- and
