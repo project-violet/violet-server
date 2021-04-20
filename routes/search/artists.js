905c2597200ae35cff2e26a7ee82530278d05b7c
violet-dev <violet.dev.master@gmail.com>
2021-04-20 07:22:37 +0000

    (search-artists) Add language option

--- routes/search/artists.js
+++ routes/search/artists.js
@@ -15,7 +15,15 @@ const artistsSchema = Joi.object({
   Series: Joi.array().items(Joi.string()),
   Characters: Joi.array().items(Joi.string()),
   Tags: Joi.array().items(Joi.string()),
+  Language: Joi.array().min(1).items(Joi.string()).required(),
 });
+
+/*
+select a.Article, 'michiking' as Artist from (eharticles_artists_junction as a
+left join eharticles as b on a.Article=b.Id) where a.Artist=( select Id from
+eharticles_artists where Name='michiking' ) and (b.Language='korean' or
+b.Language='n/a') order by Article desc limit 1
+*/
 
 function _artists(res, query) {
   var sql_union = [];
@@ -23,56 +31,66 @@ function _artists(res, query) {
   if ('Artists' in query) {
     query.Artists.forEach(
         e => sql_union.push(
-            '(select Article, ? as Artist from eharticles_artists_junction where Artist=(' +
+            '(select a.Article, ? as Artist from (eharticles_artists_junction  as a left join eharticles as b on a.Article=b.Id) where a.Artist=(' +
             'select Id from eharticles_artists where Name=?' +
+            ') and (' + query.Language.map(e => 'b.Language=?').join(' or ') +
             ') order by Article desc limit 1)'));
     query.Artists.forEach(e => {
       values.push('artist:' + e);
       values.push(e);
+      query.Language.forEach(e => values.push(e));
     });
   }
   if ('Groups' in query) {
     query.Groups.forEach(
         e => sql_union.push(
-            '(select Article, ? as Artist from eharticles_groups_junction where Group=(' +
+            '(select a.Article, ? as Artist from (eharticles_groups_junction  as a left join eharticles as b on a.Article=b.Id) where a.Group=(' +
             'select Id from eharticles_groups where Name=?' +
+            ') and (' + query.Language.map(e => 'b.Language=?').join(' or ') +
             ') order by Article desc limit 1)'));
     query.Groups.forEach(e => {
       values.push('group:' + e);
       values.push(e);
+      query.Language.forEach(e => values.push(e));
     });
   }
   if ('Series' in query) {
     query.Series.forEach(
         e => sql_union.push(
-            '(select Article, ? as Artist from eharticles_series_junction where Series=(' +
+            '(select a.Article, ? as Artist from (eharticles_series_junction as a left join eharticles as b on a.Article=b.Id) where a.Series=(' +
             'select Id from eharticles_series where Name=?' +
+            ') and (' + query.Language.map(e => 'b.Language=?').join(' or ') +
             ') order by Article desc limit 1)'));
     query.Series.forEach(e => {
-      values.push('series'+e);
+      values.push('series' + e);
       values.push(e);
+      query.Language.forEach(e => values.push(e));
     });
   }
   if ('Characters' in query) {
     query.Characters.forEach(
         e => sql_union.push(
-            '(select Article, ? as Artist from eharticles_characters_junction where Character=(' +
+            '(select a.Article, ? as Artist from (eharticles_characters_junction  as a left join eharticles as b on a.Article=b.Id) where a.Character=(' +
             'select Id from eharticles_characters where Name=?' +
+            ') and (' + query.Language.map(e => 'b.Language=?').join(' or ') +
             ') order by Article desc limit 1)'));
     query.Characters.forEach(e => {
       values.push('character:' + e);
       values.push(e);
+      query.Language.forEach(e => values.push(e));
     });
   }
   if ('Tags' in query) {
     query.Tags.forEach(
         e => sql_union.push(
-            '(select Article, ? as Artist from eharticles_tags_junction where Tag=(' +
+            '(select a.Article, ? as Artist from (eharticles_tags_junction  as a left join eharticles as b on a.Article=b.Id) where a.Tag=(' +
             'select Id from eharticles_tags where Name=?' +
+            ') and (' + query.Language.map(e => 'b.Language=?').join(' or ') +
             ') order by Article desc limit 1)'));
     query.Tags.forEach(e => {
       values.push('tag:' + e);
       values.push(e);
+      query.Language.forEach(e => values.push(e));
     });
   }
 
