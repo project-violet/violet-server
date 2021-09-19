22e58db9f746d12b5324f145fde2fdfee3cd4a86
violet-dev <violet.dev.master@gmail.com>
2021-09-19 02:21:31 +0000

    restore excomment

--- memory/excomments.js
+++ memory/excomments.js
@@ -4,41 +4,41 @@
 const fs = require('fs');
 
 // id, time, author, body
-// const jsonFile = fs.readFileSync('excomment-zip.json', 'utf8');
-// const exComments = JSON.parse(jsonFile);
+const jsonFile = fs.readFileSync('excomment-zip.json', 'utf8');
+const exComments = JSON.parse(jsonFile);
 
 function _find(what) {
   var results = [];
-  // var words = what.split(' ');
-  // Object.keys(exComments).forEach(function(key) {
-  //   exComments[key].forEach(function(comment) {
-  //     if (words.every((e) => comment.Item3.includes(e)))
-  //       results.push({
-  //         id: key,
-  //         time: comment.Item1,
-  //         author: comment.Item2,
-  //         body: comment.Item3
-  //       });
-  //   });
-  // });
-  // results.sort((x, y) => y.id - x.id);
+  var words = what.split(' ');
+  Object.keys(exComments).forEach(function(key) {
+    exComments[key].forEach(function(comment) {
+      if (words.every((e) => comment.Item3.includes(e)))
+        results.push({
+          id: key,
+          time: comment.Item1,
+          author: comment.Item2,
+          body: comment.Item3
+        });
+    });
+  });
+  results.sort((x, y) => y.id - x.id);
   return results;
 }
 
 function _findByAuthor(author) {
   var results = [];
-  // Object.keys(exComments).forEach(function(key) {
-  //   exComments[key].forEach(function(comment) {
-  //     if (comment.Item2 == author)
-  //       results.push({
-  //         id: key,
-  //         time: comment.Item1,
-  //         author: comment.Item2,
-  //         body: comment.Item3
-  //       });
-  //   });
-  // });
-  // results.sort((x, y) => y.id - x.id);
+  Object.keys(exComments).forEach(function(key) {
+    exComments[key].forEach(function(comment) {
+      if (comment.Item2 == author)
+        results.push({
+          id: key,
+          time: comment.Item1,
+          author: comment.Item2,
+          body: comment.Item3
+        });
+    });
+  });
+  results.sort((x, y) => y.id - x.id);
   return results;
 }
 
