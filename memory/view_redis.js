c47aa6a26b5dee4817945b708363a95ab3c1f16a
violet-dev <violet.dev.master@gmail.com>
2021-09-27 23:05:15 +0000

    view: Fix parse error

--- memory/view_redis.js
+++ memory/view_redis.js
@@ -1,23 +1,41 @@
 // This source code is a part of Project Violet.
 // Copyright (C) 2021. violet-team. Licensed under the Apache-2.0 License.
 
-const {promisify} = require('util');
+const { promisify } = require("util");
 
-const a_database = require('../api/database');
-const a_syncdatabase = require('../api/syncdatabase');
+const a_database = require("../api/database");
+const a_syncdatabase = require("../api/syncdatabase");
 
-const logger = require('../etc/logger');
-const redis = require('../api/redis');
-const redis_sub = require('../api/redis_sub');
+const logger = require("../etc/logger");
+const redis = require("../api/redis");
+const redis_sub = require("../api/redis_sub");
 
-redis_sub.psubscribe('*').then(function(e) {
-  redis_sub.on('pmessage', function(pattern, message, channel) {
+// https://stackoverflow.com/questions/175739/built-in-way-in-javascript-to-check-if-a-string-is-a-valid-number
+function isNumeric(str) {
+  if (typeof str != "string") return false // we only process strings!  
+  return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+         !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
+}
+
+redis_sub.psubscribe("*").then(function (e) {
+  redis_sub.on("pmessage", function (pattern, message, channel) {
     console.log(message, channel);
-    if (message.toString().startsWith('__keyevent') &&
-        message.toString().endsWith('expired')) {
+    if (
+      message.toString().startsWith("__keyevent") &&
+      message.toString().endsWith("expired")
+    ) {
       // This method must called only one per keyevent.
-      logger.info('expired %s', channel);
-      redis.zincrby(channel.split('-')[0], -1, channel.split('-')[1]);
+      logger.info("expired %s", channel);
+      var type = channel.split("-")[0];
+      var id = channel.split("-")[1];
+      if (
+        (type == "weekly" ||
+        type == "daily" ||
+        type == "monthly") &&
+        isNumeric(id)
+      ) {
+        redis.zincrby(type, -1, id);
+      }
     }
   });
 });
@@ -40,58 +58,64 @@ redis_sub.psubscribe('*').then(function(e) {
 
 const zrevrangeAsync = promisify(redis.zrevrange).bind(redis);
 async function query(group, offset, count) {
-  var query = await zrevrangeAsync(group, offset, count, 'withscores');
+  var query = await zrevrangeAsync(group, offset, count, "withscores");
   var result = [];
   for (var i = 0; i < query.length; i += 2)
-    result.push([parseInt(query[i]), parseInt(query[i+1])]);
+    result.push([parseInt(query[i]), parseInt(query[i + 1])]);
   return result;
 }
 
-var CURRENT_TIMESTAMP = { toSqlString: function() { return 'CURRENT_TIMESTAMP()'; } };
+var CURRENT_TIMESTAMP = {
+  toSqlString: function () {
+    return "CURRENT_TIMESTAMP()";
+  },
+};
 
 module.exports = {
-  append: function(no, userid) {
+  append: function (no, userid) {
     try {
-      logger.info('view-append %d %s', no, userid);
+      logger.info("view-append %d %s", no, userid);
       //append(no);
     } catch (e) {
-      logger.error('view-append');
+      logger.error("view-append");
       logger.error(e);
       console.log(e);
     }
 
     var pool = a_database();
     pool.query(
-        'INSERT INTO viewtotal SET ?', {
-          ArticleId: no,
-          TimeStamp: CURRENT_TIMESTAMP,
-          UserAppId: userid,
-        },
-        function(error, results, fields) {
-          if (error != null) {
-            logger.error('viewdb', error);
-          }
-        });
+      "INSERT INTO viewtotal SET ?",
+      {
+        ArticleId: no,
+        TimeStamp: CURRENT_TIMESTAMP,
+        UserAppId: userid,
+      },
+      function (error, results, fields) {
+        if (error != null) {
+          logger.error("viewdb", error);
+        }
+      }
+    );
   },
 
-  query: function(offset, count, type) {
+  query: function (offset, count, type) {
     if (count > 10000) {
       resolve(null);
       return;
     }
-    return new Promise(async function(resolve, reject) {
+    return new Promise(async function (resolve, reject) {
       switch (type) {
-        case 'daily':
-          resolve(await query('daily', offset, count));
+        case "daily":
+          resolve(await query("daily", offset, count));
           break;
-        case 'week':
-          resolve(await query('weekly', offset, count));
+        case "week":
+          resolve(await query("weekly", offset, count));
           break;
-        case 'month':
-          resolve(await query('monthly', offset, count));
+        case "month":
+          resolve(await query("monthly", offset, count));
           break;
-        case 'alltime':
-          resolve(await query('alltime', offset, count));
+        case "alltime":
+          resolve(await query("alltime", offset, count));
           break;
       }
       resolve(null);
