8cba711fd51c0e489d4426c3cd045c2965878b18
violet-dev <violet.dev.master@gmail.com>
2021-09-28 08:53:06 +0000

    artist-comment: Fix write api

--- routes/community/anon/artistcomment/write.js
+++ routes/community/anon/artistcomment/write.js
@@ -18,7 +18,7 @@ const writeSchema = Joi.object({
 var rateLimit = [];
 
 function diff_minutes(dt2, dt1) {
-  var diff = (dt2.getTime() - dt1.getTime()) / 1000;
+  var diff = (dt2 - dt1) / 1000;
   diff /= 60;
   return Math.abs(Math.round(diff));
 }
