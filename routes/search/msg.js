1dc67409c8c832973c8ec7077d33629ea1726eb1
violet-dev <violet.dev.master@gmail.com>
2021-10-04 22:42:16 +0000

    search-msg: apply config

--- routes/search/msg.js
+++ routes/search/msg.js
@@ -1,8 +1,12 @@
 // This source code is a part of Project Violet.
 // Copyright (C) 2021. violet-team. Licensed under the Apache-2.0 License.
 
-const r_auth = require("../../auth/auth");
 const request = require("request");
+const config = require('config');
+
+const r_auth = require("../../auth/auth");
+
+const msgHost = config.get('message.host') || 'http://127.0.0.1:8864';
 
 module.exports = async function (req, res, next) {
 //   if (!r_auth.wauth(req)) {
@@ -11,7 +15,7 @@ module.exports = async function (req, res, next) {
 //   }
 
   request(
-    "http://127.0.0.1:8864" + req.path.substr(4),
+    msgHost + req.path.substr(4),
     (error, response, body) => {
       try {
         res.status(200).type("json").send(body);
