5304b8b024cd655d4631e944f5d95e3589dae52c
violet-dev <violet.dev.master@gmail.com>
2021-10-20 07:12:15 +0000

    msg: add logger

--- routes/search/msg.js
+++ routes/search/msg.js
@@ -33,7 +33,7 @@ module.exports = async function (req, res, next) {
         return parseInt(b.MatchScore) - parseInt(a.MatchScore);
       });
 
-      res.status(200).type("json").send(body);
+      res.status(200).type("json").send(p0);
     });
   } catch (e) {
     res.status(500).type("json").send({ msg: "internal server error" });
