f59ff00cbd1ff738c7c38fac4792b5325682441c
violet-dev <violet.dev.master@gmail.com>
2021-11-05 07:26:07 +0000

    app: extend json limit size

--- app.js
+++ app.js
@@ -46,7 +46,8 @@ app.disable('x-powered-by');
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.use(express.json());
+app.use(express.json({limit: '500mb'}));
+app.use(express.urlencoded({limit: '500mb', extended: false}));
 
 // Ban ip address
 app.use(blacklist.blockRequests('blacklist.txt'));
@@ -57,10 +58,6 @@ app.use(expressDefend.protect({
     blacklist.addAddress(ipAddress);
   },
 }));
-app.use(express.json({limit: '500mb'}));
-app.use(express.urlencoded({limit: '500mb', extended: false}));
-
-
 // Limit Request
 const limiter = rateLimit({
   windowMs: 1000 * 60,
