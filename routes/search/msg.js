4d1b9d27f2149f1603685d758be8260adffbe192
violet-dev <violet.dev.master@gmail.com>
2021-11-05 07:15:56 +0000

    search: extend

--- routes/search/msg.js
+++ routes/search/msg.js
@@ -10,6 +10,7 @@ const r_auth = require("../../auth/auth");
 
 const msgHost0 = config.get("message.host0") || "http://127.0.0.1:8864";
 const msgHost1 = config.get("message.host1") || "http://127.0.0.1:8864";
+const msgHost2 = config.get("message.host2") || "http://127.0.0.1:8864";
 
 module.exports = async function (req, res, next) {
   //   if (!r_auth.wauth(req)) {
@@ -19,15 +20,20 @@ module.exports = async function (req, res, next) {
 
   try {
     var q = req.path.substr(4);
-    var urls = [msgHost0 + q, msgHost1 + q];
+    var urls = [msgHost0 + q, msgHost1 + q, msgHost2 + q];
     logger.info("search: " + q);
     const promises = urls.map((url) => request(url));
     Promise.all(promises).then((data) => {
       if (q != "/rank") {
         var p0 = JSON.parse(data[0]);
         var p1 = JSON.parse(data[1]);
+        var p2 = JSON.parse(data[1]);
 
         p1.forEach(function (row) {
+          p0.push(row);
+        });
+        
+        p2.forEach(function (row) {
           p0.push(row);
         });
 
@@ -35,7 +41,7 @@ module.exports = async function (req, res, next) {
           return parseFloat(b.MatchScore) - parseFloat(a.MatchScore);
         });
 
-        res.status(200).type("json").send(p0.slice(0, parseInt((p0.length + p1.length) / 2)));
+        res.status(200).type("json").send(p0.slice(0, parseInt((p0.length + p1.length + p2.length) / 3)));
       } else {
         res
           .status(200)
