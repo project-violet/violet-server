9c0bbecea30b8dd90b6c74d7e65bd87a1a33ae30
violet-dev <violet.dev.master@gmail.com>
2021-11-08 10:24:25 +0000

    bookmarks: rename bookmarks api to bookmarks2

--- app.js
+++ app.js
@@ -78,7 +78,7 @@ app.use('/top_recent', r_top_recent);
 app.use('/record', r_record);
 app.post('/upload', r_upload);
 app.get('/restore', r_restore);
-app.get('/bookmarks', r_bookmarks);
+app.get('/bookmarks2', r_bookmarks);
 app.post('/view', r_view);
 app.get('/view', function(req, res, next) {
   res.status(405).type('html').send(p.p405);
