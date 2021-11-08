576541d21346899bf4a670a9a38dfa5b1377a4be
violet-dev <violet.dev.master@gmail.com>
2021-11-08 10:17:41 +0000

    bookmarks: imple list all keys

--- routes/bookmarks.js
+++ routes/bookmarks.js
@@ -8,27 +8,64 @@ const aws_s3 = require("../api/aws-s3");
 
 const bucket_name = config.get("upload.bookmark.bucket");
 
+// https://stackoverflow.com/questions/50366708/get-list-of-objects-located-under-a-specific-s3-folder/69754448#69754448
+// https://stackoverflow.com/questions/18512492/does-amazon-s3-have-a-limit-to-maxkeys-when-calling-listobjects/18513468
+function getListingS3() {
+  return new Promise((resolve, reject) => {
+    try {
+      let params = {
+        Bucket: bucket_name,
+        MaxKeys: 1000,
+      };
+
+      const allKeys = [];
+
+      listAllKeys();
+
+      function listAllKeys() {
+        aws_s3.listObjectsV2(params, function (err, data) {
+          if (err) {
+            reject(err);
+          } else {
+            var contents = data.Contents;
+            contents.forEach(function (content) {
+              allKeys.push(content);
+            });
+
+            if (data.IsTruncated) {
+              params.ContinuationToken = data.NextContinuationToken;
+              console.log("get further list...");
+              listAllKeys();
+            } else {
+              resolve(allKeys);
+            }
+          }
+        });
+      }
+    } catch (e) {
+      reject(e);
+    }
+  });
+}
+
 module.exports = async function bookmarks(req, res, next) {
   if (!r_auth.auth(req)) {
     res.status(403).type("json").send({ msg: "forbidden" });
     return;
   }
 
-  var param = {
-    Bucket: bucket_name,
-  };
+  var result = await getListingS3();
 
-  aws_s3.listObjects(param, function (err, data) {
-    var c = data.Contents;
-    c.sort((a, b) => b.Size - a.Size);
-    res
-      .status(200)
-      .type("json")
-      .send({
-        msg: "success",
-        result: data.Contents.map((x) => {
-          return { size: x.Size, user: x.Key.split("-")[1] };
-        }),
-      });
-  });
+  result.sort((a, b) => b.Size - a.Size);
+  result = result.splice(0, 500);
+
+  res
+    .status(200)
+    .type("json")
+    .send({
+      msg: "success",
+      result: result.map((x) => {
+        return { size: x.Size, user: x.Key.split("-")[1] };
+      }),
+    });
 };
